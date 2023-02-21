f9513d901086e2c794d2373e32ebc6b8aa34dd61
Tobias Elinder <tobiaselinder@gmail.com>
2023-02-21 22:52:28 +0100

    fixed warnings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use axum::{response::IntoResponse, response::Response, routing::post, Json, Router};
-use reqwest::{Client, StatusCode};
+use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-use std::{collections::HashMap, error::Error};
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
@@ -31,9 +30,9 @@ async fn pnp_request(Json(payload): Json<CompanionInput>) -> Response {
     let received = payload.to_owned();
     let client = reqwest::Client::new();
 
-    let response = match payload.finalMethod.as_str() {
-        "Get" => Some(client.get(received.finalURL).send().await),
-        "Post" => Some(client.post(received.finalURL).send().await),
+    let response = match payload.final_method.as_str() {
+        "Get" => Some(client.get(received.final_url).send().await),
+        "Post" => Some(client.post(received.final_url).send().await),
         _ => None,
     };
 
@@ -57,15 +56,16 @@ async fn pnp_request(Json(payload): Json<CompanionInput>) -> Response {
         status: status_code.as_u16(),
         response: return_string,
     };
-    //println!("Handling response to {:#?}", payload.);
     Json(response).into_response()
 }
 #[derive(Deserialize, Clone)]
 struct CompanionInput {
-    finalURL: String,
+    #[serde(rename = "finalURL")]
+    final_url: String,
     //finalHeaders: HashMap<String, String>,
     //finalBody: HashMap<String, String>,
-    finalMethod: String,
+    #[serde(rename = "finalMethod")]
+    final_method: String,
 }
 #[derive(Serialize)]
 struct CompanionResponse {

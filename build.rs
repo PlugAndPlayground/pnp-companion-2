d00db54d81df6fb4a7577f97216fe7f4339e77ce
Tobias Elinder <tobiaselinder@gmail.com>
2023-02-26 18:43:29 +0100

    icon for windows

---
+++ build.rs
@@ -0,0 +1,11 @@
+// build.rs
+
+extern crate winres;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("resources/pnp.ico");
+        res.compile().unwrap();
+    }
+}

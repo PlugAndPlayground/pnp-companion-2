86b19fdbe069157df9c4cbfc419fe678b78e8514
Tobias Elinder <tobiaselinder@gmail.com>
2023-11-04 23:04:45 +0100

    tray icon options not working a lil bit annoying

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 use axum::{routing::post, Router};
 
 use std::net::SocketAddr;
 use tokio::signal;
 use tower_http::cors::CorsLayer;
-use tray_icon::menu::Menu;
+use tray_icon::menu::{Menu, MenuItem, PredefinedMenuItem};
 use tray_icon::{Icon, TrayIconBuilder};
 mod response_handler;
 
@@ -16,38 +16,22 @@ async fn main() {
     start_server().await;
 }
 
-fn start_tray_icon() {
-    //std::thread::spawn(|| {
-    let img = image::load_from_memory(ICON).unwrap().into_rgba8();
-    let (width, height) = img.dimensions();
-    let icon = Icon::from_rgba(img.into_raw(), width as u32, height as u32).unwrap();
-    //let icon = Icon::from_rgba(ICON.to_vec(), 16, 16).unwrap();
-    let menu = Menu::new();
-    let _tray_icon = TrayIconBuilder::new()
-        .with_tooltip(format!("PNP companion running on port {}", PORT))
-        .with_icon(icon)
-        //.with_menu(Box::new(menu))
-        .build()
-        .unwrap();
-
-    // Create the menu
-    //});
-}
-
 async fn start_server() {
     let img = image::load_from_memory(ICON).unwrap().into_rgba8();
     let (width, height) = img.dimensions();
     let icon = Icon::from_rgba(img.into_raw(), width as u32, height as u32).unwrap();
-    //let icon = Icon::from_rgba(ICON.to_vec(), 16, 16).unwrap();
     let menu = Menu::new();
+    // TODO FIX TRAY ICON OPENING https://github.com/tauri-apps/tray-icon/issues/89
+    menu.append(&PredefinedMenuItem::quit(Some("Quit")))
+        .unwrap();
+
     let _tray_icon = TrayIconBuilder::new()
         .with_tooltip(format!("PNP companion running on port {}", PORT))
         .with_icon(icon)
-        //.with_menu(Box::new(menu))
+        .with_menu(Box::new(menu))
         .build()
         .unwrap();
 
-    //start_tray_icon(); // TODO FIX THIS
     tracing_subscriber::fmt::init();
     println!("Starting server on: {}", PORT);
 

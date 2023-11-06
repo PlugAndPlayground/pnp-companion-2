a3d081302e81c7bb561a6e625f4a7e0a411e20cf
Tobias Elinder <tobiaselinder@gmail.com>
2023-11-06 20:23:57 +0100

    re-added tray icon for windows

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 use axum::{routing::post, Router};
 
 use std::net::SocketAddr;
 use tower_http::cors::CorsLayer;
-//use tray_icon::menu::{Menu, PredefinedMenuItem};
-//use tray_icon::{Icon, TrayIconBuilder};
+use tray_icon::menu::{Menu, PredefinedMenuItem};
+use tray_icon::{Icon, TrayIconBuilder};
 mod response_handler;
 
-//static ICON: &'static [u8] = include_bytes!("../resources/pnp.png");
+static ICON: &'static [u8] = include_bytes!("../resources/pnp.png");
 static PORT: u16 = 6655;
+
+fn create_tray_icon() -> tray_icon::TrayIcon {
+    let img = image::load_from_memory(ICON).unwrap().into_rgba8();
+    let (width, height) = img.dimensions();
+    let icon = Icon::from_rgba(img.into_raw(), width as u32, height as u32).unwrap();
+    let menu = Menu::new();
+    // TODO FIX TRAY ICON OPENING https://github.com/tauri-apps/tray-icon/issues/89
+
+    return TrayIconBuilder::new()
+        .with_tooltip(format!("PNP companion running on port {}", PORT))
+        .with_icon(icon)
+        .with_menu(Box::new(menu))
+        .build()
+        .unwrap();
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     // TODO fix tray icon, it crashes on mac
-    /*let img = image::load_from_memory(ICON).unwrap().into_rgba8();
-    let (width, height) = img.dimensions();
-    let icon = Icon::from_rgba(img.into_raw(), width as u32, height as u32).unwrap();
-    //let menu = Menu::new();
-    // TODO FIX TRAY ICON OPENING https://github.com/tauri-apps/tray-icon/issues/89
-
-    let _tray_icon = TrayIconBuilder::new()
-        .with_tooltip(format!("PNP companion running on port {}", PORT))
-        .with_icon(icon)
-        .build()
-        .unwrap();
-
-    */
-
+    #[cfg(target_os = "windows")]
+    let _tray_icon = create_tray_icon();
     tokio::task::spawn(start_server());
 
     // Capture Ctrl+C to shutdown gracefully
@@ -37,8 +40,6 @@ async fn main() {
             println!("Received Ctrl+C. Shutting down...");
         }
     }
-    // Explicitly drop the tray icon
-    //drop(_tray_icon);
 
     // Exit the program
     std::process::exit(0);
